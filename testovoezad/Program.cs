3577978ad71c4525d5cbcf235fe7e91c521eab30
danilgarilla <desar-reper@mail.ru>
2022-07-23 18:14:28 +0700

    Добавьте файлы проекта.

---
+++ testovoezad/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace Testovoe
+{
+    class Program 
+    {
+       
+
+
+        static void Main()
+        {
+            Console.WriteLine("Enter number 1");
+            int a = int.Parse(Console.ReadLine() ) ;
+
+            Console.WriteLine("Enter number 2");
+            int b = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine( "+, - , / , *" );
+            string c = Console.ReadLine();
+
+
+            switch (c){
+                case "+":
+                      Console.WriteLine( a + b);
+                    break;
+
+                    case "-":
+                    Console.WriteLine( a - b);
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Error");
+                     }
+                    else
+                       Console.WriteLine (a / b);
+                    break;
+
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+
+
+                    
+
+
+            }
+
+            
+
+
+
+   
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
